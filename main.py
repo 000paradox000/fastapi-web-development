e877ad5582a5fc63d1705a9c49155d25e7ac1dde
Ariel Calzada <ariel.calzada@gmail.com>
2023-05-01 16:58:20 -0500

    Add item model and routes.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from decimal import Decimal
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: Decimal
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def index():
@@ -14,4 +21,19 @@ def index():
 def say_hello(name: str):
     return {
         "message": f"Hello {name}"
+    }
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int, q: Union[str, None] = None):
+    return {
+        "item_id": item_id,
+        "q": q,
+    }
+
+@app.put("/items/{item_id}")
+def put_item(item_id: int, item: Item):
+    return {
+        "item_id": item_id,
+        "name": item.name,
+        "price": item.price,
     }
